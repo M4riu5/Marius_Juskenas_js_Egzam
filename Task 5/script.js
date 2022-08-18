8f3cd171b7f3f63265c84b887a85ad9581082341
Marius <mariushaf@gmail.com>
2022-08-18 10:59:38 +0300

    test

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,11 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+function filterDogOwers(arr) {
+
+}
+
+function filterAdults(arr) {
+  asd
+}
